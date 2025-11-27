5e8d0c977f269fb1acc356227ca3393732593017
veber01 <102132093+veber01@users.noreply.github.com>
2025-11-27 18:10:41 +0100

    Update QuickMenuManager.cs

--- Patches/QuickMenuManager.cs
+++ Patches/QuickMenuManager.cs
@@ -1,19 +1,21 @@
 using HarmonyLib;
-
-namespace LateCompany.Patches;
-
+namespace ExtendedLateCompany.Patches;
 [HarmonyPatch(typeof(QuickMenuManager), nameof(QuickMenuManager.DisableInviteFriendsButton))]
-internal static class DisableInviteFriendsButton_Patch {
+internal static class DisableInviteFriendsButton_Patch
+{
 	[HarmonyPrefix]
-	private static bool Prefix() { return false; }
+	private static bool Prefix()
+	{
+		return false;
+	}
 }
-
 [HarmonyPatch(typeof(QuickMenuManager), nameof(QuickMenuManager.InviteFriendsButton))]
-internal static class InviteFriendsButton_Patch {
+internal static class InviteFriendsButton_Patch
+{
 	[HarmonyPrefix]
-	private static bool Prefix() {
-		if (Plugin.LobbyJoinable && !GameNetworkManager.Instance.disableSteam) GameNetworkManager.Instance.InviteFriendsUI();
-
+	private static bool Prefix()
+	{
+		if (ExtendedLateCompany.LobbyJoinable && !GameNetworkManager.Instance.disableSteam) GameNetworkManager.Instance.InviteFriendsUI();
 		return false;
 	}
 }
